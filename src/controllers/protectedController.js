22954949a58a6ac7316ed873f3bf626a284a703c
Eduard Baescu <eduard@geev.com>
2025-09-19 16:52:41 +0200

    Back - Format

--- src/controllers/protectedController.js
+++ src/controllers/protectedController.js
@@ -3,12 +3,10 @@ const protectedService = require("../services/protectedService");
 const tokenCheck = async (req, res) => {
   const tokenCheck = protectedService.tokenCheck();
   if (req.role == "admin") {
-    res
-      .status(200)
-      .json({
-        message: `Protected route accessed. Your role is ${req.role}`,
-        tokenCheck,
-      });
+    res.status(200).json({
+      message: `Protected route accessed. Your role is ${req.role}`,
+      tokenCheck,
+    });
   } else {
     res.status(500).send({ status: "FAILED", data: { error: "Acces denied" } });
   }
